a0a34b99f60b189a45640ce20747a3e4fd0a818f
frozo <sergitto.l@gmail.com>
2015-11-13 19:11:49 -0500

    ParcialFinal
    EntregaCodioFuente

---
+++ src/co/edu/umb/ingenieria/software/tp/Empresa.java
@@ -0,0 +1,71 @@
+package co.edu.umb.ingenieria.software.tp;
+
+public class Empresa {
+	 private String nit;
+	 private String nombre;
+	 private String razonComercial;
+	 private String direccion;
+	 private int telefono;
+	 private static Empresa miEmpresa;
+	 
+	 public  static Empresa getEmpresa(String nit,String nombre, String razonComercial, String direccion, int telefono) {
+	 
+	 if (miEmpresa==null) {
+	 
+		 miEmpresa=new Empresa(nit, nombre, razonComercial, direccion, telefono);
+	 }
+	 return miEmpresa;
+	 }
+	 
+	 private Empresa(String nit,String nombre, String razonComercial, String direccion, int telefono)
+	 {
+	 
+	 this.nit=nit;
+	 this.nombre=nombre;
+	 this.razonComercial=razonComercial;
+	 this.direccion=direccion;
+	 this.telefono=telefono;
+	 
+	 }
+
+	public String getNit() {
+		return nit;
+	}
+
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getRazonComercial() {
+		return razonComercial;
+	}
+
+	public void setRazonComercial(String razonComercial) {
+		this.razonComercial = razonComercial;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	 
+}
---
+++ src/co/edu/umb/ingenieria/software/tp/Principal.java
@@ -0,0 +1,17 @@
+package co.edu.umb.ingenieria.software.tp;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		 Empresa e = Empresa.getEmpresa("858585-1", "Lilis S.A", "Lili engeniers", "calle falsa 123", 2565281);
+		 
+		 System.out.println(e.getNit());		 
+		 System.out.println(e.getNombre());
+		 System.out.println(e.getRazonComercial());
+		 System.out.println(e.getDireccion());
+		 System.out.println(e.getTelefono());
+
+	}
+
+}
